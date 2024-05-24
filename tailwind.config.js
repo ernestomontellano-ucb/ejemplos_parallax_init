20b70431f8f9479a179a4c9cbeea5097b43f5eaa
Ernesto Montellano <emontellano@ucb.edu.bo>
2024-05-24 16:02:14 -0400

    css completo

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,9 @@ module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
     backgroundImage: {
-      "fondo1": "url(../ejemplos/imgs/0034.png')",
-      "fondo2": "url(../ejemplos/imgs/0034_mod.png')",
-      "fondocierre": "url(../ejemplos/imgs/cierre.webp')",
+      "fondo1": "url(../ejemplos/imgs/0034.png)",
+      "fondo2": "url(../ejemplos/imgs/0034_mod.png)",
+      "fondocierre": "url(../ejemplos/imgs/cierre.webp)",
     },
 
     extend: {},
